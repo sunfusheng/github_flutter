34d1c4ca77e52d4cbbb59dd59917511a8d9a416c
sunfusheng <sunfusheng1989@126.com>
2020-12-27 21:35:46 +0800

    升级Flutter版本

--- android/app/src/main/kotlin/com/sunfusheng/github/flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/sunfusheng/github/flutter/MainActivity.kt
@@ -2,15 +2,15 @@ package com.sunfusheng.github.flutter
 
 import android.os.Build
 import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.annotation.Nullable
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.statusBarColor = 0
         }
-        GeneratedPluginRegistrant.registerWith(this)
     }
 }
